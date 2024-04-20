b598144da5c841ba33132cc7058cd2aeabfeaeae
Rudra Narola <114010315+RudraNarola@users.noreply.github.com>
2024-04-20 16:51:02 +0530

    theme update

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,12 @@ import React from "react";
 
 const HomePage: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-50 p-5">
-      <h1 className="text-4xl font-bold text-gray-800 text-center shadow-2xl p-5 font-serif rounded-lg">
-        Welcome to the Home Page!
-      </h1>
-      <VideoPlayer name="Control Flow in Python - If Elif Else Statements.mp4"/>
-
-      {/* <video controls>
-        <source
-          src="https://firebasestorage.googleapis.com/v0/b/clipcognition.appspot.com/o/Control%20Flow%20in%20Python%20-%20If%20Elif%20Else%20Statements.mp4?alt=media&token=03ebadc2-e398-4f93-b303-3e1f118c195e"
-          type="video/mp4"
-        />
-        Your browser does not support the video tag.
-      </video> */}
+    <div className="flex flex-col items-center justify-center h-full">
+      <h1 className="text-white text-5xl">Welcome to Clip Cognition</h1>
+      {/* <VideoPlayer name="Control Flow in Python - If Elif Else Statements.mp4"/> */}
     </div>
   );
 };
+5;
 
 export default HomePage;
--- components/header.tsx
+++ components/header.tsx
@@ -1,25 +1,24 @@
-import { UserButton } from '@clerk/nextjs';
-import Link from 'next/link';
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 import { auth, currentUser } from "@clerk/nextjs/server";
 export default async function Header() {
   const { userId } = auth();
 
   return (
-    <div className='bg-gray-600 text-neutral-100'>
-      <div className='container mx-auto flex items-center justify-between py-4'>
-        <Link href='/'>Home</Link>
+    <div className=" text-neutral-100">
+      <div className="container mx-auto flex items-center justify-between py-4">
+        <Link href="/">Home</Link>
         <div>
           {userId ? (
-            <div className='flex gap-4 items-center'>
-              <Link href='/dashboard'>Dashboard</Link>
+            <div className="flex gap-4 items-center">
+              <Link href="/dashboard">Dashboard</Link>
               {/* <Link href='/upload'>Upload</Link> */}
-              <UserButton afterSignOutUrl='/' />
+              <UserButton afterSignOutUrl="/" />
             </div>
           ) : (
-            <div className='flex gap-4 items-center'>
-            
-              <Link href='/sign-up'>Sign up</Link>
-              <Link href='/sign-in'>Sign In</Link>
+            <div className="flex gap-4 items-center">
+              <Link href="/sign-up">Sign up</Link>
+              <Link href="/sign-in">Sign In</Link>
             </div>
           )}
         </div>
