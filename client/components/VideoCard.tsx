c2a0a1ef5db92c4c321ef4fb6db4213f62534cc2
Jaivik-kalathiya <jaivikkalathiya@gmail.com>
2024-04-21 04:09:37 +0530

    repeat

--- client/components/VideoCard.tsx
+++ client/components/VideoCard.tsx
@@ -42,7 +42,8 @@ const VideoCard: React.FC<VideoCardProps> = ({ title, rating, previewImage }) =>
     <div className="bg-neutral-600 rounded-lg shadow-md overflow-hidden relative group">
     <img src={previewImage} alt={title} className="w-full h-40 object-cover" />
     {/* <Play /> */}
-    <FaPlayCircle className="text-black text-6xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-200" />
+    
+    {/* <FaPlayCircle className="text-black text-6xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-200" /> */}
     <div className="p-4">
       <h3 className="text-lg font-bold">{title}</h3>
       <div className="flex items-center mt-2">{renderStars(rating)}</div>
